57c754dbc583ad88d925f6429f71dc326361e70a
Juli2017 <juli@hoa-cpa.com>
2018-05-28 19:07:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png
 }).addTo(map);
 
 // Edit to upload GeoJSON data file from your local directory
-$.getJSON("NewMedianGrossRentByZCTA.geojson", function (data) {
+$.getJSON("GrossRentByZCTASMC.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
     style: style,
     onEachFeature: onEachFeature
